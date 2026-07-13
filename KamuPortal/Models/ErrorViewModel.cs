a1945baf202b0fd93394c61626e704f56aaff293
GamzeKizill <gmzkzl33@gmail.com>
2026-07-13 15:16:55 +0300

    Initial ASP.NET Core MVC setup

---
+++ KamuPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KamuPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
